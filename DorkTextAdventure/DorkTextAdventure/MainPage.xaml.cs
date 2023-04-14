a7c49577106d6e28278b66920cc8f695e0cb69ec
SilverSong3 <eehlash@gmail.com>
2023-04-13 20:39:57 -0400

    Add project files.

---
+++ DorkTextAdventure/DorkTextAdventure/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using DorkTextAdventure.Models;
+
+namespace DorkTextAdventure
+{
+    public partial class MainPage : ContentPage
+    {
+
+        List<Room> rooms = new List<Room>();
+        
+
+        public MainPage()
+        {
+            int roomID = 0;
+            Room r = new Room();
+            r.ID = 0;
+            r.Name = "First Floor";
+            r.Description = "You are in a damp and cold hotel. The lights flicker ominously, and you see no one around you.";
+            //r.Items[0] = 0;
+            
+            rooms.Add(r);
+
+
+
+            //InitializeComponent();
+
+            Editor editor = new Editor()
+            {
+                Text = "Dork Adventure 0.01\n\nBy: El Slash" + rooms[roomID].ToString(),
+                FlowDirection = FlowDirection.LeftToRight,
+                HeightRequest = 500,
+                WidthRequest = 100,
+                IsReadOnly = true,
+                MinimumWidthRequest = 100,
+                FontSize = 20,
+                TextColor = Color.White,
+                Background = new SolidColorBrush(Color.Black),
+                BackgroundColor = Color.Black,
+                VerticalOptions = LayoutOptions.Center,
+                IsTabStop = false,
+            };
+
+            Entry entry = new Entry
+            {
+                HeightRequest = 70,
+                WidthRequest = 100,
+                //MinimumWidthRequest = 100,
+                FontSize = 40,
+                TextColor = Color.White,
+                BackgroundColor = Color.DarkSlateBlue,
+            };
+
+
+            Button button = new Button
+            {
+                Text = "Submit",
+                HeightRequest = 70,
+                WidthRequest = 50,
+                FontSize = 40,
+                TextColor = Color.White,
+                //Background = new SolidColorBrush(Color.CornflowerBlue),
+                BackgroundColor = Color.CornflowerBlue,
+                IsTabStop = false,
+            };
+
+            //main page view
+            StackLayout stackLayout = new StackLayout
+            {
+                Children = {
+                    editor,
+                    entry,
+                    button,
+                },
+                WidthRequest = 700,
+                HeightRequest = 1500,
+                BackgroundColor = Color.Gray,
+                HorizontalOptions = LayoutOptions.Center,
+            };
+
+            
+
+            entry.Completed += async (sender, args) =>
+            {
+                await DisplayAlert("command: success", "great job", "Okay!");
+            };
+
+            button.Clicked += async (sender, args) => 
+            {
+                entry.Text = "";
+                await Action();
+                //eh, there's something that can make the thing focused.
+            };
+
+
+            Content = stackLayout;
+
+
+        }
+
+
+        public async Task Action()
+        {
+            await DisplayAlert("You pressed a button. Good job.", "...great job", "Okay...?");
+        }
+
+
+
+    }
+}
---
+++ DorkTextAdventure/DorkTextAdventure/Models/Room.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace DorkTextAdventure.Models
+{
+    public class Room
+    {
+        //north = 0, east, south, west, up, down, No exit = -1
+        public int ID { get; set; }
+        public string Name { get; set; }    
+        public int Level { get; set; }
+        public string Description { get; set; }
+
+
+        public int[] Exits { get; set; }
+        public int[] Items { get; set; }
+
+
+        public Room()
+        {
+            Exits = new int[0];
+            Items = new int[0];
+        }
+
+        public string CreateExitString()
+        {
+            string exits = "";
+
+            if (Exits.Count() == 0)
+            {
+                exits = "Nowhere, you are trapped...?";
+            }
+
+            foreach (int i in Exits)
+            {
+                //TODO: look up exits from id...thing
+            }
+            return exits;
+        }
+
+        public string CreateItemsString()
+        {
+            string items = "";
+
+            if (Items.Count() == 0)
+            {
+                items = "There is nothing to see here.";
+            }
+
+
+            foreach (int i in Items)
+            {
+                //TODO: look up item from ID.
+            }
+            return items;
+        }
+
+
+        public override string ToString()
+        {
+            string exits = CreateExitString();
+            string items = CreateItemsString();
+
+            return $"{Name}: {ID}\n\n{Description}\n\n" +
+                $"You can go: {exits}\n\n" +
+                $"Laying on the floor: {items}\n\n\n" +
+                $"What do you wish to do? ";
+        }
+    }
+}
